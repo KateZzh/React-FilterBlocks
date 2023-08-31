afa5ecdf3c639e09bff1d42be9d7c884ebb10067
KateZzh <ekaterin.pro2@gmail.com>
2023-08-31 23:14:25 +0300

    VacancyPage

--- client/src/page/JobPage/JobPage.jsx
+++ client/src/page/JobPage/JobPage.jsx
@@ -2,42 +2,16 @@ import { Input, Button, Pagination } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 import style from "./jobPage.module.scss";
 import { Link } from "react-router-dom";
+import array from "../../storage/storage.json";
 
 const JobPage = () => {
-  const array = [
-    {
-      header: "Менеджер-дизайнер",
-      salary: "з/п от 70000 rub",
-      city: "Новый Уренгой",
-      jobTime: "Полный рабочий день",
-    },
-    {
-      header: "Ведущий графический дизайнер НЕ УДАЛЕННО",
-      salary: "з/п от 80000 rub",
-      city: "Москва",
-      jobTime: "Полный рабочий день",
-    },
-    {
-      header: "Работник декорации, дизайнер (ТЦ Амбар)",
-      salary: "з/п 29000 rub",
-      city: "Самара",
-      jobTime: "Сменный график работы",
-    },
-    {
-      header: "Менеджер-дизайнер",
-      salary: "з/п 55000 - 95000 rub",
-      city: "Тюмень",
-      jobTime: "Полный рабочий день",
-    },
-  ];
-
   return (
     <>
       <Input placeholder="Your email" rightSection={<Button>Поиск</Button>} icon={<IconSearch />} />
 
       <div>
         {array.map((el, index) => (
-          <Link to={`/vacancy/${el.header}`} key={index}>
+          <Link to={`/vacancy/${el.id}`} key={index}>
             <div className={style.wrapper} key={index}>
               <h2>{el.header}</h2>
 
--- client/src/page/VacancyPage/VacancyPage.jsx
+++ client/src/page/VacancyPage/VacancyPage.jsx
@@ -1,11 +1,69 @@
 import { useParams } from "react-router-dom";
+import array from "../../storage/storage.json";
+import { useState, useEffect } from "react";
+import style from "./VacancyPage.module.scss";
 
 const VacancyPage = () => {
   const { id } = useParams();
+  const [cardInfo, setCardInfo] = useState({});
+
+  function filtered() {
+    const res = array.filter((el) => el.id == id);
+
+    setCardInfo(res[0]);
+  }
+
+  useEffect(() => {
+    filtered();
+  }, []);
 
   return (
     <>
-      <h1>{id}</h1>
+      <div className={style.wrapper}>
+        <h2>{cardInfo.header}</h2>
+
+        <div className={style.info}>
+          <div>{cardInfo.salary}</div>
+          <p>{cardInfo.time}</p>
+        </div>
+
+        <div className={style.location}>
+          <div className={style.logoLocation}></div>
+          <p>{cardInfo.city}</p>
+        </div>
+      </div>
+
+      <div className={style.cardWrapper}>
+        <div className={style.responsibilities}>
+          <h2>Обязанности:</h2>
+
+          <ul>
+            {cardInfo.responsibilities
+              ? cardInfo.responsibilities.map((el, index) => <li key={index}>{el}</li>)
+              : null}
+          </ul>
+        </div>
+
+        <div className={style.requirements}>
+          <h2>Требования:</h2>
+
+          <ul>
+            {cardInfo.requirements
+              ? cardInfo.requirements.map((el, index) => <li key={index}>{el}</li>)
+              : null}
+          </ul>
+        </div>
+
+        <div className={style.conditions}>
+          <h2>Условия:</h2>
+
+          <ul>
+            {cardInfo.conditions
+              ? cardInfo.conditions.map((el, index) => <li key={index}>{el}</li>)
+              : null}
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
