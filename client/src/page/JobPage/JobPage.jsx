7343c07aff7cc9a0d755f9a4866493c614631aea
KateZzh <ekaterin.pro2@gmail.com>
2023-09-02 02:48:18 +0300

    pagination

--- client/src/page/JobPage/JobPage.jsx
+++ client/src/page/JobPage/JobPage.jsx
@@ -4,8 +4,18 @@ import style from "./jobPage.module.scss";
 import { Link } from "react-router-dom";
 import array from "../../storage/storage.json";
 import Header from "../../components/Header/Header";
+import { useState } from "react";
 
 const JobPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const amountCards = 6;
+  const npage = Math.ceil(array.length / amountCards);
+  const lastIndex = amountCards * currentPage;
+  const firstIndex = lastIndex - amountCards;
+  const partArray = array.slice(firstIndex, lastIndex);
+  const emptyRow = amountCards - (array.length % amountCards);
+
   return (
     <>
       <Header />
@@ -18,14 +28,14 @@ const JobPage = () => {
         />
 
         <div>
-          {array.map((el, index) => (
+          {partArray.map((el, index) => (
             <Link to={`/vacancy/${el.id}`} key={index}>
               <div className={style.wrapperJob} key={index}>
                 <h2>{el.header}</h2>
 
                 <div className={style.info}>
                   <div>{el.salary}</div>
-                  <p>{el.jobTime}</p>
+                  <p>{el.time}</p>
                 </div>
 
                 <div className={style.location}>
@@ -37,7 +47,15 @@ const JobPage = () => {
           ))}
         </div>
 
-        <Pagination total={3} />
+        {array.length % amountCards !== 0 && currentPage === npage ? (
+          <div style={{ height: 155 * emptyRow }}></div>
+        ) : null}
+
+        <Pagination
+          className={style.pagination}
+          total={npage}
+          onChange={(val) => setCurrentPage(val)}
+        />
       </div>
     </>
   );
